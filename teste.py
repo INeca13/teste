c2b72a1a4197ac71f18ff3b1624c529381236fc1
INeca13 <i.neca@campus.fct.unl.pt>
2022-12-13 21:03:04 +0000

    p

---
+++ teste.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import csv
+import itertools
+import zipfile
+import tsfel
+import scipy
+
+#pip install tsfel
+#pip3 install matplotlib
+#pip3 install seaborn
+#pip install scipy==1.8.1
